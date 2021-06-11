7e42b11f8a38ebb6e72ba1efdea063a85341e4b1
logotip010 <logotip010@mail.ru>
2021-06-11 13:33:07 +0300

    Добавил валидацию

--- Teledoc/wwwroot/js/main.js
+++ Teledoc/wwwroot/js/main.js
@@ -2,30 +2,50 @@
     el: '#app',
     data: {
         founders: [
-                
+
         ],
         founder: {
-            INN: '',
-            Name: '',
-            Surname: '',
-            Patronymic: ''
+            INN: null,
+            Name: null,
+            Surname: null,
+            Patronymic: null
         },
         selected: 'Entity',
+        validated: true,
+
     },
     methods: {
         AddFounderToList(founder) {
-            if (this.selected === 'SP' && this.founders.length < 1) {
+            if (this.IsNull(founder.INN) && this.IsNull(founder.Name) && this.IsNull(founder.Surname) && this.IsNull(founder.Patronymic)) {
+                if (this.selected === 'SP' && this.founders.length < 1) {
 
-                this.founders.push(founder)
-                this.founder = {}
+                    this.founders.push(founder)
+                    this.founder = {}
+                }
+                else if (this.selected === 'Entity') {
+                    this.founders.push(founder)
+                    this.founder = {}
+                }
             }
-            else if (this.selected === 'Entity') {
-                this.founders.push(founder)
-                this.founder = {}
+            else {
+
+                this.validated = false
+
             }
+
         },
         deleteFounder(founder) {
             this.founders.splice(this.founders.indexOf(founder), 1)
+
         },
+        IsNull(value) {
+            if (value != '' && value != null && value != NaN) {
+                return true
+            }
+
+            else {
+                return false
+            }
+        }
     },
 })
